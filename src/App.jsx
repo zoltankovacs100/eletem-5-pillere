5187b4e078cfae7585a634cd20c6e06bb0f4763a
zoltankovacs100 <zoltankovacs100@gmail.com>
2025-08-03 15:40:16 +0200

    refactor: Change green theme to blue

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function App() {
             height="60"
             patternUnits="userSpaceOnUse"
           >
-            <rect width="2" height="60" fill="#017a76" />
-            <rect x="2" width="4" height="60" fill="#01918C" />
+            <rect width="2" height="60" fill="#8Ea4b8" />
+            <rect x="2" width="4" height="60" fill="#A9C0D3" />
           </pattern>
         </defs>
 
